1d930ec4abeccd8de97434e648d1c5fdba380af6
Vox <61358692+voxdotdev@users.noreply.github.com>
2021-03-06 11:28:52 -0600

    new file - first class, main entry

--- Entry.cs
+++ Entry.cs
@@ -2,5 +2,13 @@
 {
     public class Entry
     {
+        /* This is a project, there are many like it but this one is mine.
+         Within this project I have sucessfully created a Github repo, committed changes and created branches.
+         For my next trick, i'll create a simple script that does something 
+         to organize my unholy amount of bird photos. Group photos via primary color of object in center?
+          Request classification from user via drop down selection of breed type?
+          Do none of this and instead write a script that turns off my computer every time I look up pictures of cats?
+          We'll see.  
+         */
     }
 }
